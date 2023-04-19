5fb9187954268dd991e639ea3d784f3b700f8383
Mateusz Zgardowski <m.zgardowski@global4net.com>
2023-04-19 20:36:59 +0200

    main/init

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const MainPage = () => {
+  return (
+    <View>
+      <Text>New App</Text>
+    </View>
+  );
+};
+
+export default MainPage;
